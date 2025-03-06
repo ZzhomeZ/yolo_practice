e1924016a5249097a45723b542d78a1c2b38f90c
ZzhomeZ <1297828269@qq.com>
2025-03-06 20:30:40 +0800

    基础训练

--- lianxi.py
+++ lianxi.py
@@ -2,3 +2,10 @@ from ultralytics import YOLO
 
 model = YOLO('yolov10n.pt')
 # model('A.jpg',show=True,save=True)   # 检测图片，show表示展示，save表示保存
+
+# model.train(data='data.yaml',epochs=50,batch=16,imgsz=640,device='cpu')  # 开始训练模型
+
+# model.val(data='data.yaml', batch=16)   # 开始验证模型
+
+a = YOLO('runs/detect/train3/weights/best.pt')  # 使用训练好的模型
+a.predict('xun1/images/test', show=True, save=True)  # 检测文件夹中的图片
